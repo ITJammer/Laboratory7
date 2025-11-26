0cfa0066e92ff485f63378b7f50c5ae7d8442201
ITJammer <meldrelyn20@gmail.com>
2025-11-26 17:29:53 +0800

    lab-8

---
+++ src/main/java/com/espelimbergo/lab_7/entity/Book.java
@@ -0,0 +1,15 @@
+package com.espelimbergo.lab_7.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private Long id;
+    private String title;
+    private int pages;
+    private String author;
+}
